aa3b7365fb7ee9a1c67dc3a2acc80f3ac8a6a7da
jdcarter157 <85311538+jdcarter157@users.noreply.github.com>
2022-09-14 10:35:50 -0400

    adding hisgh and low values for the chart

--- main.py
+++ main.py
@@ -25,10 +25,18 @@ mycursor = mydb.cursor()
 dir_path = r'/Users/jordan_carter/Downloads/jim_jsons/data'
 
 ### select coin
-
-cname = "BITMEX:LTCUSD"
-
-#### find length of coin
+cname ="BITMEX:ETHUSD_ETH"
+# cname = ["BITMEX:LTCUSD","BITMEX:BLTC","BITMEX:ETHUSD_ETH"]
+# c=0
+# if c<=len(cname):
+#     sql1 = "SELECT COUNT(*) FROM coin_data where cname = %s"
+#     sql2 = (cname[c])
+#     mycursor.execute(sql1, sql2)
+#     myresult = mycursor.fetchall()
+#     numofrows = myresult[0][0]
+#     print("number of rows-->", numofrows)
+#     c=c+1
+# for c in cname:
 sql1 = "SELECT COUNT(*) FROM coin_data where cname = %s"
 sql2 = (cname,)
 mycursor.execute(sql1, sql2)
@@ -56,24 +64,22 @@ for x in range(0, numofrows):
     low = 10000
     thislow = 0
     for x in range(0, 10):
-        thislow = float(myresult[x][3])
-        if (thislow < low):
-            low = thislow
+            thislow = float(myresult[x][3])
+            if (thislow < low):
+                low = thislow
+                theid = myresult[x][0]
+                sql1 = "UPDATE coin_data SET lows = %s where id = %s "
+                sql2 = (low, theid)
+                mycursor.execute(sql1, sql2)
+                mydb.commit()
 
-    theid = myresult[x][0]
-
-    sql1 = "UPDATE coin_data SET lows = %s where id = %s "
-    sql2 = (low, theid)
-    mycursor.execute(sql1, sql2)
-    mydb.commit()
-
-    print("low:", low)
-    print("the id:", theid)
+                print("low:", low)
+                print("the id:", theid)
 
 ### ---------------------------- highs!!!!!!!!!!!!
 
-baseplus = 0
-goback = 10
+                baseplus = 0
+                goback = 10
 for x in range(0, numofrows):
     baseplus = numofrows + goback
     sql1 = " SELECT * FROM coin_data WHERE cname = %s LIMIT %s , %s"
@@ -87,19 +93,18 @@ for x in range(0, numofrows):
     high = 0
     thishigh = 0
     for x in range(0, 10):
-        thishigh = myresult[x][3]
+        thishigh = float(myresult[x][3])
         if (thishigh > high):
             high = thishigh
             print(" new high : ", high)
+            theid = myresult[x][0]
+            print("theid:", theid)
+            sql1 = "UPDATE coin_data SET highs = %s where id = %s "
+            sql2 = (high, theid)
+            mycursor.execute(sql1, sql2)
+            mydb.commit()
 
-    theid = myresult[x][0]
-    print("theid:", theid)
-    sql1 = "UPDATE coin_data SET highs = %s where id = %s "
-    sql2 = (high, theid)
-    mycursor.execute(sql1, sql2)
-    mydb.commit()
-
-    print("low:", low)
-    print("the id:", theid)
+            print("high", high)
+            print("the id:", theid)
 
 ### --------------------------------------
